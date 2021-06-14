3110b79f63c525f64a95d8638104d84e269c0ef0
realTobby <tkattanek00@gmail.com>
2021-06-14 15:08:04 +0200

    Cutting the images

--- PKMN-TCG-Scraper/Program.cs
+++ PKMN-TCG-Scraper/Program.cs
@@ -19,37 +19,62 @@ namespace PKMN_TCG_Scraper
 
         static void Main(string[] args)
         {
-            System.IO.Directory.CreateDirectory("images");
-
-            int itemsDownloaded = 0;
-
-            for(int pageIndex = 36; pageIndex < maxPage; pageIndex++)
+            if(args.Contains("-c"))
             {
-                var url = baseUrl + pageIndex + urlParameters;
-                var web = new HtmlWeb();
-                var doc = web.Load(url);
+                System.IO.Directory.CreateDirectory("images/cutouts");
 
-                // get div entry-content
-                // get a card-image-link
-                // get a card-title-link
+                var files = System.IO.Directory.GetFiles("images");
 
-                ////div[contains(@class,'myclass')]
-                Console.WriteLine("--- PAGE: " + pageIndex);
-                foreach (HtmlNode node in doc.DocumentNode.SelectNodes("//article[contains(@class, 'type-pkmn_card entry')]"))
+                Bitmap CroppedImage;
+                Bitmap source;
+                foreach (string imagePath in files)
                 {
-                    string imageLinkDirect = node.FirstChild.FirstChild.FirstChild.GetAttributeValue("href", "notfound");
-                    string cardName = node.FirstChild.LastChild.FirstChild.LastChild.FirstChild.FirstChild.FirstChild.InnerHtml;
-                    Console.WriteLine("### DOWNLOAD CARD: " + cardName + "  FROM: " + imageLinkDirect);
-                    SaveImage(imageLinkDirect, "images/" + cardName + ".jpeg", ImageFormat.Jpeg);
-                    itemsDownloaded++;
+                    int x = 58, y = 100, width = 616, height = 383;
+                    source = new Bitmap(imagePath);
+                    CroppedImage = source.Clone(new System.Drawing.Rectangle(x, y, width, height), PixelFormat.Format24bppRgb);
+                    string filename = System.IO.Path.GetFileNameWithoutExtension(imagePath) + "cut.jpeg";
+                    Console.WriteLine("### CUTTING: " + filename);
+                    CroppedImage.Save("images/cutouts/" + filename);
+                    CroppedImage.Dispose();
+                    source.Dispose();
+                }
+            }
+            else
+            {
+                System.IO.Directory.CreateDirectory("images");
+
+                int itemsDownloaded = 0;
+
+                for (int pageIndex = 51; pageIndex < maxPage; pageIndex++)
+                {
+                    var url = baseUrl + pageIndex + urlParameters;
+                    var web = new HtmlWeb();
+                    var doc = web.Load(url);
+
+                    // get div entry-content
+                    // get a card-image-link
+                    // get a card-title-link
+
+                    ////div[contains(@class,'myclass')]
+                    Console.WriteLine("--- PAGE: " + pageIndex);
+                    foreach (HtmlNode node in doc.DocumentNode.SelectNodes("//article[contains(@class, 'type-pkmn_card entry')]"))
+                    {
+                        string imageLinkDirect = node.FirstChild.FirstChild.FirstChild.GetAttributeValue("href", "notfound");
+                        string cardName = node.FirstChild.LastChild.FirstChild.LastChild.FirstChild.FirstChild.FirstChild.InnerHtml;
+                        Console.WriteLine("### DOWNLOAD CARD: " + cardName + "  FROM: " + imageLinkDirect);
+                        SaveImage(imageLinkDirect, "images/" + cardName + ".jpeg", ImageFormat.Jpeg);
+                        itemsDownloaded++;
+                    }
+
                 }
 
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("=== FINISHED DOWNLOADING " + itemsDownloaded + " ITEMS");
             }
 
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("=== FINISHED DOWNLOADING " + itemsDownloaded + " ITEMS");
+            
 
             Console.ReadLine();
 
